6553bac0de83341de5a563185a9a7ef8d0e65147
Arthur Vinicius <thuur.vss@gmail.com>
2023-02-02 21:49:53 -0300

    feat: listagem com foto funcionando

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,65 @@
-import { useEffect, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { People } from "types/IPeople";
-import { DataGrid, GridColDef, GridRowsProp } from "@mui/x-data-grid";
+import {
+  DataGrid,
+  GridColDef,
+  GridRenderCellParams,
+  GridRowsProp,
+} from "@mui/x-data-grid";
+import uuid from "react-uuid";
+import Image from "next/image";
+import { Button } from "@mui/material";
+
+interface Row {
+  picture: string;
+  id: string;
+  name: string;
+  email: string;
+  cell: string;
+}
 
 export default function Home() {
-  const [listaDePessoas, setListaDePessoas] = useState<People[]>([]);
+  const [listaDePessoas, setListaDePessoas] = useState<Row[]>([]);
 
   useEffect(() => {
     handleGetPeople();
   }, []);
 
   async function handleGetPeople() {
-    const response = await fetch("https://randomuser.me/api/?results=1000");
+    const response = await fetch("https://randomuser.me/api/?results=5000");
     const data = await response.json();
     const pessoas: People[] = data.results;
-    console.log(pessoas);
-    setListaDePessoas(pessoas);
+
+    const rows = pessoas.map((p) => {
+      return {
+        id: p.id.value !== null ? p.id.value : uuid(),
+        picture: p.picture.thumbnail,
+        name: p.name.first,
+        email: p.email,
+        cell: p.cell,
+      };
+    });
+
+    setListaDePessoas(rows);
   }
 
   const columns: GridColDef[] = [
-    { field: "picture", headerName: "Picture", width: 150 },
+    {
+      field: "picture",
+      headerName: "Picture",
+      width: 150,
+      renderCell: (link) => <img src={link.row.picture} alt={""} />,
+    },
     { field: "id", headerName: "No.", width: 150 },
     { field: "name", headerName: "Name", width: 150 },
     { field: "email", headerName: "Email", width: 150 },
     { field: "cell", headerName: "Cell", width: 150 },
   ];
 
-  const rows: GridRowsProp = [
-    { id: 1, name: "Hello", email: "World" },
-    { id: 2, name: "DataGridPro", email: "is Awesome" },
-    { id: 3, name: "MUI", email: "is Amazing" },
-  ];
-
   return (
     <>
-      {/* {listaDePessoas.map((p) => {
-        return <p key={p.id.value}>{p.name.first}</p>;
-      })} */}
-      <div style={{ height: 300, width: "100%" }}>
-        <DataGrid rows={rows} columns={columns} />
+      <div style={{ height: "100vh", width: "100%" }}>
+        <DataGrid rows={listaDePessoas} columns={columns} />
       </div>
     </>
   );
